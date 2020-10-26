c23bb9d6cf811f89a9effe6f8e4f3b9f07975bc5
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-10-26 16:12:58 +0100

    updated checkWinner

--- backup.js
+++ backup.js
@@ -3,12 +3,9 @@ cells.forEach((element) => element.addEventListener('click', fireOnCells));
 
 // memory-------------
 let currentPlayer = 1;
-let winForX = 0;
-let winForO = 0;
-let draws = 0;
 let gameTurnsLeft = 9;
 
-// let gameOn = true;
+let gameOn = true;
 // -------------------
 
 function fireOnCells(event) {
@@ -21,54 +18,11 @@ function fireOnCells(event) {
       event.target.textContent = 'x';
       //   update the remaining turns
       gameTurnsLeft--;
-      // console.log(gameTurnsLeft);
-      let winner = checkWinner();
-      if (winner === 'X') {
-        console.log('X WON!');
-        winForX++;
-        const showWin = document.querySelector('#winX');
-        showWin.textContent = `${winForX}`;
-        // adding class to animate
-        document.querySelector('#score-x').classList.add('animate__rubberBand');
-        cells.forEach((cell) => {
-          cell.textContent = '';
-        });
-        currentPlayer = 1;
-        // removing animation class
-        setInterval(() => {
-          document
-            .querySelector('#score-x')
-            .classList.remove('animate__rubberBand');
-        }, 2000);
-        // resetting games turns
-        gameTurnsLeft = 9;
-      } else {
-        currentPlayer = 0;
-      }
+      console.log(gameTurnsLeft);
+      currentPlayer = 0;
 
-      // checking for draws
-      if (gameTurnsLeft === 0 && !winner) {
-        let draw = document.querySelector('#draw');
-        draws++;
-        draw.textContent = draws;
-        // animation
-        document
-          .querySelector('#score-draw')
-          .classList.add('animate__rubberBand');
-        // cleaning cells
-        cells.forEach((cell) => {
-          cell.textContent = '';
-        });
-        currentPlayer = 1;
-        // removing animation class
-        setInterval(() => {
-          document
-            .querySelector('#score-draw')
-            .classList.remove('animate__rubberBand');
-        }, 2000);
-        // resetting games turns
-        gameTurnsLeft = 9;
-      }
+      //  cant win before turn 3
+      console.log('winner: ', checkWinner());
     }
 
     // play as X
@@ -85,55 +39,19 @@ function fireOnCells(event) {
     } else {
       // im clicking on a valid cell, so let's conquer it!
       event.target.textContent = 'o';
-
-      // check winner
-      let winner = checkWinner();
-      if (winner === 'O') {
-        console.log('O WON!');
-        winForO++;
-        const showWin = document.querySelector('#winO');
-        showWin.textContent = `${winForO}`;
-
-        // adding class to animate
-        document.querySelector('#score-o').classList.add('animate__rubberBand');
-        cells.forEach((cell) => {
-          cell.textContent = '';
-        });
-        setInterval(() => {
-          document
-            .querySelector('#score-o')
-            .classList.remove('animate__rubberBand');
-        }, 2000);
-        currentPlayer = 1;
-      } else {
-        currentPlayer = 1;
-      }
       //   update the remaining turns
       gameTurnsLeft--;
+      console.log(gameTurnsLeft);
+      currentPlayer = 1;
+      console.log('winner: ', checkWinner());
 
-      // checking for draws
-      if (gameTurnsLeft === 0 && !winner) {
-        let draw = document.querySelector('#draw');
-        draws++;
-        draw.textContent = draws;
-        // animation
-        document
-          .querySelector('#score-draw')
-          .classList.add('animate__rubberBand');
-        // cleaning cells
-        cells.forEach((cell) => {
-          cell.textContent = '';
-        });
-        currentPlayer = 1;
-        // removing animation class
-        setInterval(() => {
-          document
-            .querySelector('#score-draw')
-            .classList.remove('animate__rubberBand');
-        }, 2000);
-        // resetting games turns
-        gameTurnsLeft = 9;
-      }
+      /*  cant win before turn 3
+        if (gameTurnsLeft >= 4) {
+          //checkWinner()
+        } else {
+          //pass the turn
+          currentPlayer = 0;
+        } */
     }
   }
 }
@@ -146,18 +64,22 @@ function fireOnCells(event) {
 
 // we assume we start with X
 
+// Click => end turn => change symbol
+// i need to store the value of the previous click in memory & check/update each turn
+
 // cant click on the other symbols or i'll change them
 
 // check if there are 3 winning symbols, if so end the game
 
 function checkWinner() {
+  // take the reference to the array
+
   const cellValues = document.querySelectorAll('.cell');
   // console.log(cellValues);
 
-  // try with arrays
-  let rowARR = [];
-  let colARR = [];
-  let diagARR = [];
+  let rows = '';
+  let cols = '';
+  let diagonals = '';
 
   let winner = '';
 
@@ -171,7 +93,6 @@ function checkWinner() {
   // i need to iterate 3 times (3 rows) so the condition must be i < i +3
   // the last iteration will give me i = 9, which i dont need, so i break
   for (let i = 0; i < i + 3; i += 3) {
-    let rows = '';
     if (i === 9) {
       break;
     } else {
@@ -180,21 +101,19 @@ function checkWinner() {
 
         // check for empty strings to separate the string
         // if a cell has not been clicked i need to concat an empty space otherwise will mess with the concatenation of elements, for example |o|x|x|
-        // ---------------------------------------------|x| | | will be considered as 3 cosecutive x's, and i don't want that because later on i'll check if that string includes 'xxx' => 3 consecutive x's
+        // -------------------------------------------------------|x| | | will be considered as 3 cosecutive x's, and i don't want that because later on i'll check if that string includes 'xxx' => 3 consecutive x's
         // by checking the empty string i avoid this behavior
         if (cellValues[j].textContent === '') {
           rows += ' ';
         } else {
           // concatenate the x or o
-
           rows += cellValues[j].textContent;
         }
       }
     }
-    rowARR.push(rows);
   }
 
-  console.log(rowARR);
+  console.log('rows', rows);
 
   // same thing for columns but i need inner while loop (with a counter) to better point out how many times it must run
 
@@ -202,8 +121,6 @@ function checkWinner() {
   // --------------------|3|4|5|
   // --------------------|6|7|8|
   for (let i = 0; i < 3; i++) {
-    let cols = '';
-
     let counter = 0;
     let j = i;
     // i create j = i to say to my while loop in which col must start looking
@@ -220,10 +137,8 @@ function checkWinner() {
         counter++;
       }
     }
-    colARR.push(cols);
   }
-  console.log(colARR);
-  // console.log('COLUMNS:', cols);
+  console.log('COLUMNS:', cols);
 
   // check diagonals
   // i need to run the outer loop for 2 times for the diagonals
@@ -232,8 +147,6 @@ function checkWinner() {
   // --------------------|6| |8|
   // diag1 start at index 0 and diag2 start at index 2(ergo i+2)
   for (let i = 0; i <= 2; i += 2) {
-    let diagonals = '';
-
     let counter = 0;
     // j to tell to the while loops in which diagonal to start
     let j = i;
@@ -263,31 +176,26 @@ function checkWinner() {
         }
       }
     }
-    diagARR.push(diagonals);
   }
-  console.log(diagARR);
 
-  // console.log('DIAGONALS:', diagonals);
+  console.log('DIAGONALS:', diagonals);
 
-  // now i check if the strings in my array contain a concatenation of winning x's or o's
+  // now i check if my strings contains 3 concatanations of x's or o's or no concatenation at all, in order to figure out the winner or the draw
   if (
-    rowARR.includes('xxx') ||
-    colARR.includes('xxx') ||
-    diagARR.includes('xxx')
+    rows.includes('xxx') ||
+    cols.includes('xxx') ||
+    diagonals.includes('xxx')
   ) {
-    winner = 'X';
+    winner = 'x';
   } else if (
-    rowARR.includes('ooo') ||
-    colARR.includes('ooo') ||
-    diagARR.includes('ooo')
+    rows.includes('ooo') ||
+    cols.includes('ooo') ||
+    diagonals.includes('ooo')
   ) {
-    winner = 'O';
+    winner = 'o';
   } else {
     winner = '';
   }
-
-  console.log('the winner is:', winner);
-  // console.log('winner of the rows:', winner);
 
   // console.log('The winner is:', winner);
   return winner;
