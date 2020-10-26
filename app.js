077ea392261b7ad75efe871a0af8bb9cbc008701
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2020-10-26 18:43:13 +0100

    fixed functions

--- app.js
+++ app.js
@@ -12,128 +12,90 @@ let gameTurnsLeft = 9;
 // -------------------
 
 function fireOnCells(event) {
-  if (currentPlayer === 1) {
-    // need check to not le click alredy clicked cells
-    if (event.target.textContent === 'o' || event.target.textContent === 'x') {
-      console.log('trying click wrong cell,do nothing');
+  // need check to not le click alredy clicked cells
+
+  if (event.target.textContent === 'o' || event.target.textContent === 'x') {
+    console.log('trying click wrong cell, do nothing');
+  } else if (currentPlayer === 1) {
+    // im clicking on a valid cell, so let's conquer it!
+    event.target.textContent = 'x';
+    //   update the remaining turns
+    gameTurnsLeft--;
+    console.log('turns LEFT: ', gameTurnsLeft);
+
+    let winner = checkWinner();
+
+    if (winner === 'X') {
+      console.log('X WON!');
+      winForX++;
+      const showWin = document.querySelector('#winX');
+      showWin.textContent = `${winForX}`;
+
+      //animate score
+      animateScore('#score-x');
+
+      // clear board
+      clearBoard();
+      // resetting games turns
+      resetTurn();
+      // resetting player
+      resetPlayer();
+    } else if (gameTurnsLeft <= 0) {
+      let draw = document.querySelector('#draw');
+      draws++;
+      draw.textContent = `${draws}`;
+      // animation
+      animateScore('#score-draw');
+      // clear board
+      clearBoard();
+      // resetting games turns
+      resetTurn();
+      // resetting player
+      resetPlayer();
     } else {
-      // im clicking on a valid cell, so let's conquer it!
-      event.target.textContent = 'x';
-      //   update the remaining turns
-      gameTurnsLeft--;
-      // console.log(gameTurnsLeft);
-      let winner = checkWinner();
-      if (winner === 'X') {
-        console.log('X WON!');
-        winForX++;
-        const showWin = document.querySelector('#winX');
-        showWin.textContent = `${winForX}`;
-        // adding class to animate
-        document.querySelector('#score-x').classList.add('animate__rubberBand');
-        cells.forEach((cell) => {
-          cell.textContent = '';
-        });
-        currentPlayer = 1;
-        // removing animation class
-        setInterval(() => {
-          document
-            .querySelector('#score-x')
-            .classList.remove('animate__rubberBand');
-        }, 2000);
-        // resetting games turns
-        gameTurnsLeft = 9;
-      } else {
-        currentPlayer = 0;
-      }
-
-      // checking for draws
-      if (gameTurnsLeft === 0 && !winner) {
-        let draw = document.querySelector('#draw');
-        draws++;
-        draw.textContent = draws;
-        // animation
-        document
-          .querySelector('#score-draw')
-          .classList.add('animate__rubberBand');
-        // cleaning cells
-        cells.forEach((cell) => {
-          cell.textContent = '';
-        });
-        currentPlayer = 1;
-        // removing animation class
-        setInterval(() => {
-          document
-            .querySelector('#score-draw')
-            .classList.remove('animate__rubberBand');
-        }, 2000);
-        // resetting games turns
-        gameTurnsLeft = 9;
-      }
+      // pass the turn
+      currentPlayer = 0;
     }
-
-    // play as X
-    // check if the game is won
-    // if so stop the game //hilight winner
-    // else change the player
   } else {
     // & play as O
-    // check if the game is won
-    // if so stop the game //hilight winner
-    // else change the player
-    if (event.target.textContent === 'o' || event.target.textContent === 'x') {
-      console.log('trying click wrong cell,do nothing');
+    event.target.textContent = 'o';
+
+    //   update the remaining turns
+    gameTurnsLeft--;
+    console.log('turns LEFT: ', gameTurnsLeft);
+
+    // check winner
+    let winner = checkWinner();
+    if (winner === 'O') {
+      // update score
+      console.log('O WON!');
+      winForO++;
+      const showWin = document.querySelector('#winO');
+      showWin.textContent = `${winForO}`;
+
+      // adding class to animate
+      animateScore('#score-o');
+      //clear the board
+      clearBoard();
+      // reset player
+      resetPlayer();
+      // reset turns
+      resetTurn();
+    } else if (gameTurnsLeft <= 0) {
+      let draw = document.querySelector('#draw');
+      draws++;
+      draw.textContent = `${draws}`;
+      // animation
+      animateScore('#score-draw');
+      // clear board
+      clearBoard();
+      // resetting games turns
+      resetTurn();
+      // resetting player
+      resetPlayer();
     } else {
-      // im clicking on a valid cell, so let's conquer it!
-      event.target.textContent = 'o';
-
-      // check winner
-      let winner = checkWinner();
-      if (winner === 'O') {
-        console.log('O WON!');
-        winForO++;
-        const showWin = document.querySelector('#winO');
-        showWin.textContent = `${winForO}`;
-
-        // adding class to animate
-        document.querySelector('#score-o').classList.add('animate__rubberBand');
-        cells.forEach((cell) => {
-          cell.textContent = '';
-        });
-        setInterval(() => {
-          document
-            .querySelector('#score-o')
-            .classList.remove('animate__rubberBand');
-        }, 2000);
-        currentPlayer = 1;
-      } else {
-        currentPlayer = 1;
-      }
-      //   update the remaining turns
-      gameTurnsLeft--;
-
-      // checking for draws
-      if (gameTurnsLeft === 0 && !winner) {
-        let draw = document.querySelector('#draw');
-        draws++;
-        draw.textContent = draws;
-        // animation
-        document
-          .querySelector('#score-draw')
-          .classList.add('animate__rubberBand');
-        // cleaning cells
-        cells.forEach((cell) => {
-          cell.textContent = '';
-        });
-        currentPlayer = 1;
-        // removing animation class
-        setInterval(() => {
-          document
-            .querySelector('#score-draw')
-            .classList.remove('animate__rubberBand');
-        }, 2000);
-        // resetting games turns
-        gameTurnsLeft = 9;
-      }
+      //pass the turn
+      resetPlayer();
     }
   }
 }
@@ -170,25 +132,21 @@ function checkWinner() {
   // at every iteration of the outer loop i add +3 to go to the first element of the next row
   // i need to iterate 3 times (3 rows) so the condition must be i < i +3
   // the last iteration will give me i = 9, which i dont need, so i break
-  for (let i = 0; i < i + 3; i += 3) {
+  for (let i = 0; i < 9; i += 3) {
     let rows = '';
-    if (i === 9) {
-      break;
-    } else {
-      for (let j = i; j < i + 3; j++) {
-        // with the inner loop i check the values of cell in that row which start at index j=i and increments +1 the get the next adjacent element
+    for (let j = i; j < i + 3; j++) {
+      // with the inner loop i check the values of cell in that row which start at index j=i and increments +1 the get the next adjacent element
 
-        // check for empty strings to separate the string
-        // if a cell has not been clicked i need to concat an empty space otherwise will mess with the concatenation of elements, for example |o|x|x|
-        // ---------------------------------------------|x| | | will be considered as 3 cosecutive x's, and i don't want that because later on i'll check if that string includes 'xxx' => 3 consecutive x's
-        // by checking the empty string i avoid this behavior
-        if (cellValues[j].textContent === '') {
-          rows += ' ';
-        } else {
-          // concatenate the x or o
+      // check for empty strings to separate the string
+      // if a cell has not been clicked i need to concat an empty space otherwise will mess with the concatenation of elements, for example |o|x|x|
+      // ---------------------------------------------|x| | | will be considered as 3 cosecutive x's, and i don't want that because later on i'll check if that string includes 'xxx' => 3 consecutive x's
+      // by checking the empty string i avoid this behavior
+      if (cellValues[j].textContent === '') {
+        rows += ' ';
+      } else {
+        // concatenate the x or o
 
-          rows += cellValues[j].textContent;
-        }
+        rows += cellValues[j].textContent;
       }
     }
     rowARR.push(rows);
@@ -211,14 +169,12 @@ function checkWinner() {
       // check for empty strings to separate the string, same as before
       if (cellValues[j].textContent === '') {
         cols += ' ';
-        // here i upgrade j +3 because the next element in the col will be at index = index of the start col + 3
-        j += 3;
-        counter++;
       } else {
         cols += cellValues[j].textContent;
-        j += 3;
-        counter++;
       }
+      // here i upgrade j +3 because the next element in the col will be at index = index of the start col + 3
+      j += 3;
+      counter++;
     }
     colARR.push(cols);
   }
@@ -291,4 +247,43 @@ function checkWinner() {
 
   // console.log('The winner is:', winner);
   return winner;
+}
+
+function resetTurn() {
+  gameTurnsLeft = 9;
+}
+
+function resetPlayer() {
+  currentPlayer = 1;
+}
+
+function clearBoard() {
+  setTimeout(() => {
+    cells.forEach((cell) => {
+      cell.textContent = '';
+    });
+  }, 500);
+}
+// function clearBoard() {
+//   cells.forEach((cell) => {
+//     cell.textContent = '';
+//   });
+// }
+
+function animateScore(scoreToAnimate) {
+  // adding class to animate
+  document.querySelector(scoreToAnimate).classList.add('animate__rubberBand');
+
+  // removing animation class
+  setTimeout(() => {
+    document
+      .querySelector(scoreToAnimate)
+      .classList.remove('animate__rubberBand');
+  }, 2000);
+}
+
+function upadateScore(scoreCounter, idToUpdate) {
+  scoreCounter++;
+  const showWin = document.querySelector(idToUpdate);
+  showWin.textContent = scoreCounter;
 }
--- backup.js
+++ backup.js
@@ -3,55 +3,115 @@ cells.forEach((element) => element.addEventListener('click', fireOnCells));
 
 // memory-------------
 let currentPlayer = 1;
+let winForX = 0;
+let winForO = 0;
+let draws = 0;
 let gameTurnsLeft = 9;
 
-let gameOn = true;
+// let gameOn = true;
 // -------------------
 
 function fireOnCells(event) {
-  if (currentPlayer === 1) {
-    // need check to not le click alredy clicked cells
-    if (event.target.textContent === 'o' || event.target.textContent === 'x') {
-      console.log('trying click wrong cell,do nothing');
+  // need check to not le click alredy clicked cells
+
+  if (event.target.textContent === 'o' || event.target.textContent === 'x') {
+    console.log('trying click wrong cell, do nothing');
+  } else if (currentPlayer === 1) {
+    // im clicking on a valid cell, so let's conquer it!
+    event.target.textContent = 'x';
+    //   update the remaining turns
+    gameTurnsLeft--;
+    console.log('turns LEFT: ', gameTurnsLeft);
+
+    let winner = checkWinner();
+
+    if (winner === 'X') {
+      console.log('X WON!');
+      winForX++;
+      const showWin = document.querySelector('#winX');
+      showWin.textContent = `${winForX}`;
+
+      //animate score
+      animateScore('#score-x');
+
+      // clear board
+      clearBoard();
+      // resetting games turns
+      resetTurn();
+      // resetting player
+      resetPlayer();
+    } else if (gameTurnsLeft <= 0) {
+      let draw = document.querySelector('#draw');
+      draws++;
+      draw.textContent = draws;
+      // animation
+      animateScore('#score-draw');
+      // clear board
+      clearBoard();
+      // resetting games turns
+      resetTurn();
+      // resetting player
+      resetPlayer();
     } else {
-      // im clicking on a valid cell, so let's conquer it!
-      event.target.textContent = 'x';
-      //   update the remaining turns
-      gameTurnsLeft--;
-      console.log(gameTurnsLeft);
+      // pass the turn
       currentPlayer = 0;
-
-      //  cant win before turn 3
-      console.log('winner: ', checkWinner());
     }
-
-    // play as X
-    // check if the game is won
-    // if so stop the game //hilight winner
-    // else change the player
   } else {
     // & play as O
-    // check if the game is won
-    // if so stop the game //hilight winner
-    // else change the player
-    if (event.target.textContent === 'o' || event.target.textContent === 'x') {
-      console.log('trying click wrong cell,do nothing');
-    } else {
-      // im clicking on a valid cell, so let's conquer it!
-      event.target.textContent = 'o';
-      //   update the remaining turns
-      gameTurnsLeft--;
-      console.log(gameTurnsLeft);
-      currentPlayer = 1;
-      console.log('winner: ', checkWinner());
+    event.target.textContent = 'o';
 
-      /*  cant win before turn 3
-        if (gameTurnsLeft >= 4) {
-          //checkWinner()
-        } else {
-          //pass the turn
-          currentPlayer = 0;
-        } */
+    //   update the remaining turns
+    gameTurnsLeft--;
+    console.log('turns LEFT: ', gameTurnsLeft);
+
+    // check winner
+    let winner = checkWinner();
+    if (winner === 'O') {
+      // update score
+      console.log('O WON!');
+      winForO++;
+      const showWin = document.querySelector('#winO');
+      showWin.textContent = `${winForO}`;
+
+      // adding class to animate
+      document.querySelector('#score-o').classList.add('animate__rubberBand');
+      //clear the board
+      clearBoard();
+
+      setTimeout(() => {
+        document
+          .querySelector('#score-o')
+          .classList.remove('animate__rubberBand');
+      }, 2000);
+      // reset player
+      resetPlayer();
+      // reset turns
+      resetTurn();
+    } else if (gameTurnsLeft <= 0) {
+      //upadate score
+      let draw = document.querySelector('#draw');
+      draws++;
+      draw.textContent = draws;
+      // animation
+      document
+        .querySelector('#score-draw')
+        .classList.add('animate__rubberBand');
+      // cleaning cells
+      clearBoard();
+
+      // removing animation class
+      setTimeout(() => {
+        document
+          .querySelector('#score-draw')
+          .classList.remove('animate__rubberBand');
+      }, 2000);
+      // reset player
+      resetPlayer();
+      // resetting games turns
+      resetTurn();
+    } else {
+      //pass the turn
+      resetPlayer();
     }
   }
 }
@@ -64,22 +124,18 @@ function fireOnCells(event) {
 
 // we assume we start with X
 
-// Click => end turn => change symbol
-// i need to store the value of the previous click in memory & check/update each turn
-
 // cant click on the other symbols or i'll change them
 
 // check if there are 3 winning symbols, if so end the game
 
 function checkWinner() {
-  // take the reference to the array
-
   const cellValues = document.querySelectorAll('.cell');
   // console.log(cellValues);
 
-  let rows = '';
-  let cols = '';
-  let diagonals = '';
+  // try with arrays
+  let rowARR = [];
+  let colARR = [];
+  let diagARR = [];
 
   let winner = '';
 
@@ -92,28 +148,27 @@ function checkWinner() {
   // at every iteration of the outer loop i add +3 to go to the first element of the next row
   // i need to iterate 3 times (3 rows) so the condition must be i < i +3
   // the last iteration will give me i = 9, which i dont need, so i break
-  for (let i = 0; i < i + 3; i += 3) {
-    if (i === 9) {
-      break;
-    } else {
-      for (let j = i; j < i + 3; j++) {
-        // with the inner loop i check the values of cell in that row which start at index j=i and increments +1 the get the next adjacent element
+  for (let i = 0; i < 9; i += 3) {
+    let rows = '';
+    for (let j = i; j < i + 3; j++) {
+      // with the inner loop i check the values of cell in that row which start at index j=i and increments +1 the get the next adjacent element
 
-        // check for empty strings to separate the string
-        // if a cell has not been clicked i need to concat an empty space otherwise will mess with the concatenation of elements, for example |o|x|x|
-        // -------------------------------------------------------|x| | | will be considered as 3 cosecutive x's, and i don't want that because later on i'll check if that string includes 'xxx' => 3 consecutive x's
-        // by checking the empty string i avoid this behavior
-        if (cellValues[j].textContent === '') {
-          rows += ' ';
-        } else {
-          // concatenate the x or o
-          rows += cellValues[j].textContent;
-        }
+      // check for empty strings to separate the string
+      // if a cell has not been clicked i need to concat an empty space otherwise will mess with the concatenation of elements, for example |o|x|x|
+      // ---------------------------------------------|x| | | will be considered as 3 cosecutive x's, and i don't want that because later on i'll check if that string includes 'xxx' => 3 consecutive x's
+      // by checking the empty string i avoid this behavior
+      if (cellValues[j].textContent === '') {
+        rows += ' ';
+      } else {
+        // concatenate the x or o
+
+        rows += cellValues[j].textContent;
       }
     }
+    rowARR.push(rows);
   }
 
-  console.log('rows', rows);
+  console.log(rowARR);
 
   // same thing for columns but i need inner while loop (with a counter) to better point out how many times it must run
 
@@ -121,6 +176,8 @@ function checkWinner() {
   // --------------------|3|4|5|
   // --------------------|6|7|8|
   for (let i = 0; i < 3; i++) {
+    let cols = '';
+
     let counter = 0;
     let j = i;
     // i create j = i to say to my while loop in which col must start looking
@@ -128,17 +185,17 @@ function checkWinner() {
       // check for empty strings to separate the string, same as before
       if (cellValues[j].textContent === '') {
         cols += ' ';
-        // here i upgrade j +3 because the next element in the col will be at index = index of the start col + 3
-        j += 3;
-        counter++;
       } else {
         cols += cellValues[j].textContent;
-        j += 3;
-        counter++;
       }
+      // here i upgrade j +3 because the next element in the col will be at index = index of the start col + 3
+      j += 3;
+      counter++;
     }
+    colARR.push(cols);
   }
-  console.log('COLUMNS:', cols);
+  console.log(colARR);
+  // console.log('COLUMNS:', cols);
 
   // check diagonals
   // i need to run the outer loop for 2 times for the diagonals
@@ -147,6 +204,8 @@ function checkWinner() {
   // --------------------|6| |8|
   // diag1 start at index 0 and diag2 start at index 2(ergo i+2)
   for (let i = 0; i <= 2; i += 2) {
+    let diagonals = '';
+
     let counter = 0;
     // j to tell to the while loops in which diagonal to start
     let j = i;
@@ -176,27 +235,64 @@ function checkWinner() {
         }
       }
     }
+    diagARR.push(diagonals);
   }
+  console.log(diagARR);
 
-  console.log('DIAGONALS:', diagonals);
+  // console.log('DIAGONALS:', diagonals);
 
-  // now i check if my strings contains 3 concatanations of x's or o's or no concatenation at all, in order to figure out the winner or the draw
+  // now i check if the strings in my array contain a concatenation of winning x's or o's
   if (
-    rows.includes('xxx') ||
-    cols.includes('xxx') ||
-    diagonals.includes('xxx')
+    rowARR.includes('xxx') ||
+    colARR.includes('xxx') ||
+    diagARR.includes('xxx')
   ) {
-    winner = 'x';
+    winner = 'X';
   } else if (
-    rows.includes('ooo') ||
-    cols.includes('ooo') ||
-    diagonals.includes('ooo')
+    rowARR.includes('ooo') ||
+    colARR.includes('ooo') ||
+    diagARR.includes('ooo')
   ) {
-    winner = 'o';
+    winner = 'O';
   } else {
     winner = '';
   }
 
+  console.log('the winner is:', winner);
+  // console.log('winner of the rows:', winner);
+
   // console.log('The winner is:', winner);
   return winner;
+}
+
+function resetTurn() {
+  gameTurnsLeft = 9;
+}
+
+function resetPlayer() {
+  currentPlayer = 1;
+}
+
+function clearBoard() {
+  cells.forEach((cell) => {
+    cell.textContent = '';
+  });
+}
+
+function animateScore(scoreToAnimate) {
+  // adding class to animate
+  document.querySelector(scoreToAnimate).classList.add('animate__rubberBand');
+
+  // removing animation class
+  setTimeout(() => {
+    document
+      .querySelector(scoreToAnimate)
+      .classList.remove('animate__rubberBand');
+  }, 2000);
+}
+
+function upadateScore(scoreCounter, idToUpdate) {
+  scoreCounter++;
+  const showWin = document.querySelector(idToUpdate);
+  showWin.textContent = `${scoreCounter}`;
 }
